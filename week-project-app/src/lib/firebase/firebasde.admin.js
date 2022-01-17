73672081d80a0200fc3f35998c0e60e67140fb5f
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-01-17 17:44:53 +0000

    fix: .env for security

--- week-project-app/src/lib/firebase/firebasde.admin.js
+++ week-project-app/src/lib/firebase/firebasde.admin.js
@@ -1,12 +1,11 @@
 import * as firebaseAdmin from "firebase-admin";
-import data from "./school-of-code-firebase-adminsdk-6v9uh-36579c9ea4.json";
 
 if (!firebaseAdmin.apps.length) {
    firebaseAdmin.initializeApp({
       credential: firebaseAdmin.credential.cert({
-         privateKey: data.private_key,
-         clientEmail: data.client_email,
-         projectId: data.project_id,
+         privateKey: process.env.NEXT_LOCAL_PRIVATE_KEY,
+         clientEmail: process.env.NEXT_LOCAL_CLIENT_EMAIL,
+         projectId: process.env.NEXT_LOCAL_PROJECT_ID,
       }),
    });
 }
